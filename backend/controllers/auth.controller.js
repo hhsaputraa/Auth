94e4dd8a22af71da7a4cd702ed17e6a92f4e8742
hhsaputraa <hhsptr12@gmail.com>
2024-09-02 06:34:55 +0700

    update auth controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,47 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All field are required");
+    }
+
+    const userAlreadyExist = await User.findOne({ email });
+    if (userAlreadyExist) {
+      return res.status(400).json({ success: false, message: "user already exist" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiredAt: Date.now() + 24 * 60 * 60 * 1000, // expired 24hours
+    });
+
+    await user.save();
+
+    //jwt
+    generateTokenAndSetCookie(res, user._id);
+    res.status(201).json({
+      success: true,
+      message: "User creted Succesfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+export const login = async (req, res) => {
+  res.send("login route");
+};
+export const logout = async (req, res) => {
+  res.send("logout route");
+};
--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,13 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("test asd");
-});
+app.use(express.json()); // allows us to parse incoming request:req.body
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("server running on port 3000");
+  console.log("server running on port : ", PORT);
 });
